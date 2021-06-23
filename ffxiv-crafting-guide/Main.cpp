761f9b0a466ff442b05859649c8f23ad2f09a9ce
ging3399 <ging3399@foxmail.com>
2021-06-23 13:48:34 +0800

    Version infomation.

--- ffxiv-crafting-guide/Main.cpp
+++ ffxiv-crafting-guide/Main.cpp
@@ -8,7 +8,7 @@ using std::endl;
 using std::setw;
 
 int main() {
-	cout << "Final Fantasy XIV crafting guide v1.0, by Renko Usami (Anima)" << endl << endl << "Suitable for patch 5.5" << endl << endl;
+	cout << "Final Fantasy XIV crafting guide v0.0.1, by Renko Usami (Anima)" << endl << endl << "Suitable for game patch 5.5" << endl << endl;
 	cout << "Please select your language: " << endl << "1. English, 2. ¼òÌåÖĞÎÄ" << endl;
 	int language;
 	cin >> language;
